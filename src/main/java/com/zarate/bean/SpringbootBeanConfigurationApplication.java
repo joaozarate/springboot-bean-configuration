25369fe7c8c775750c0c09d4d74795c0394c9cb0
joaozarate <joaozarate@gmail.com>
2020-04-12 14:15:52 +0100

    Project created

---
+++ src/main/java/com/zarate/bean/SpringbootBeanConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.zarate.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeanConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBeanConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarate/bean/SpringbootBeanConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarate.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeanConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
